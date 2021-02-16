2cae0b2f126462534e4f70770bbbdb815014f896
emirhan sirinoglu <srnoglu98@gmail.com>
2021-02-16 19:53:24 +0300

    pthon-listing-tutorial

---
+++ listings-lesson1.py
@@ -0,0 +1,24 @@
+languages = ["python","java","javascrip,","c#"]
+result = languages
+result = type(languages)
+result = languages[2:]
+result = languages[1]
+result = languages[3]
+result = languages[0:2]
+result = languages[-1]
+result = languages[-4]
+result = len(languages)
+result = languages + ["reach"]
+
+# if blooğu==> koşul elemanları
+if ("python") in languages:
+     print("değer listenin bir elemanıdır")
+
+# döngüler
+     for x in languages:
+         print(x)
+
+del languages[0]
+result = languages
+
+print(result)
