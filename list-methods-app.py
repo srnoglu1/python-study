df433340f4db99ad698ab5555bb74f97f15343f9
emirhan sirinoglu <srnoglu98@gmail.com>
2021-02-19 20:50:11 +0300

    pthon-listing-tutorial

---
+++ list-methods-app.py
@@ -0,0 +1,69 @@
+#  ---- QUESTIONS ----
+
+name = ["Ada","Yiğit","Beril","Cansu","Kerem"]
+years = [1998,1996,1999,2001,2003]
+
+# cenk ismini listenin sonuna ekle ?
+ name.append("Cenk")
+ sonuc = name
+ print(sonuc)
+
+# nazlı ismini listenin başına ekle ?
+ name.insert(0,"Nazlı")
+ sonuc = name
+ print(sonuc)
+
+# yiğit ismini listeden siliniz ?
+ name.pop(1)
+ sonuc = name
+ print(sonuc)
+
+# liste elemanlarını ters çevirin ?
+ name.reverse()
+ sonuc = name
+ print(sonuc)
+
+# years listesini rakamsal büyüklüğe göre sıralayın ?
+ years.sort()
+ sonuc = years
+ print(years)
+
+# years dizisini en büyük ve en küçük elemanı nedir ?
+ sonuc = min(years)
+ sonuc = max(years)
+ print(sonuc)
+
+# beril ismi listenin bir elemanımıdır ?
+ sonuc = "Beril" in name
+ print(sonuc)
+
+# yiğit isminin indexi nedir ?
+ index = name.index("Yiğit")
+ name.pop(index)
+ print(index)
+
+# t = "iphoneX", "iphoneXS" karekter dizesini listeye çeviriniz ?
+ p = "IphoneX,IphoneXS"
+ sonuc = p.split(",")
+ print(sonuc)
+
+# years dizisinde kaç tane 1998 değeri vardır ?
+ print(years.count(1998))
+
+# years dizisindeki tüm elemanlarını siliniz ?
+ years.clear()
+ print(years)
+
+# Kullanıcıdan alacağınız 3 tane marka bilgisini yazdırıp bir listede saklayınız ?
+ markalar = []
+
+ marka = input("Marka: ")
+ markalar.append(marka)
+
+ marka = input("Marka: ")
+ markalar.append(marka)
+
+ marka = input("Marka: ")
+ markalar.append(marka)
+
+ print(markalar)
---
+++ list-methods.py
@@ -0,0 +1,30 @@
+ sayilar = [1,5,7,3,8,6,9,2,7,66]
+ harfler = ["a","c","u","d","s","o","y","c"]
+ sonuc = min(sayilar)
+ sonuc = max(sayilar)
+ sonuc = max(harfler)
+ sonuc = min(harfler)
+
+ #ekleme
+ sayilar.append(10) 
+ sayilar.insert(3,4)
+ sayilar.insert(-1,9)
+ sayilar.insert(len(sayilar),50)
+
+ #silme 
+ sayilar.pop()
+ sayilar.pop(0)
+ sayilar.remove(8)
+ harfler.remove("u")
+
+ #sıralama
+ sayilar.sort()
+ harfler.sort()
+ sayilar.reverse()
+
+ print(sayilar.count(7))
+ print(harfler.count("c"))
+ print(sayilar.index(66))
+ print(sayilar.clear())
+
+ print(sonuc)
