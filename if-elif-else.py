3bf783c171dbbd9093a810a59ea6f92bbee3c962
emirhan sirinoglu <srnoglu98@gmail.com>
2021-02-27 23:17:18 +0300

    conditional-state-blocks

---
+++ if-elif-else.py
@@ -0,0 +1,22 @@
+#first
+
+x = 20
+y = 28
+
+if (x>y):
+    print("x is greater than y. ")
+elif (x==y):
+    print("numbers are equal.")
+else:
+    print("y is greater than x.")
+
+#second
+
+number = int(input("number: "))
+
+if number >0 :
+    print("positive number.")
+elif number == 0:
+    print("number neutral.")
+else:
+    print("number negative.")
---
+++ if-else.py
@@ -0,0 +1,10 @@
+username = "srnoglu"
+password = 12345
+
+if (username == "srnoglu"):
+    if(password == 12345):
+        print("user login successful.")
+    else:
+        print("password entry is incorrect.")
+else:
+    print("user login is wrong.") 
